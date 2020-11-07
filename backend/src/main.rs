14fe9a61b491fbb411747f7afc05906bc74cd1fa
oScape <benoit.chassignol@iomentum.com>
2020-11-07 21:50:24 +0100

    switch to actix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,49 +1,37 @@
-use async_graphql::{
-    http::{playground_source, GraphQLPlaygroundConfig},
-    EmptyMutation, EmptySubscription, Schema,
-};
-use async_graphql_warp::{graphql, BadRequest, Response};
-use finthnif_lib::models::{Driver, QueryRoot};
-use http::StatusCode;
-use std::convert::Infallible;
-use warp::{http::Response as HttpResponse, Filter, Rejection};
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql_actix_web::{Request, Response};
+use finthnif_lib::models::{Driver, FinthnifSchema, QueryRoot};
+use std::io::Result as IoResult;
 
-#[tokio::main]
-async fn main() {
+async fn index(schema: web::Data<FinthnifSchema>, req: Request) -> Response {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_playground() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(
+            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
+        )))
+}
+
+#[actix_rt::main]
+async fn main() -> IoResult<()> {
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
         .data(Driver::default())
         .finish();
 
     println!("Playground: http://localhost:8000");
 
-    let graphql_post = graphql(schema).and_then(
-        |(schema, request): (
-            Schema<QueryRoot, EmptyMutation, EmptySubscription>,
-            async_graphql::Request,
-        )| async move { Ok::<_, Infallible>(Response::from(schema.execute(request).await)) },
-    );
-
-    let graphql_playground = warp::path::end().and(warp::get()).map(|| {
-        HttpResponse::builder()
-            .header("content-type", "text/html")
-            .body(playground_source(GraphQLPlaygroundConfig::new("/")))
-    });
-
-    let routes = graphql_playground
-        .or(graphql_post)
-        .recover(|err: Rejection| async move {
-            if let Some(BadRequest(err)) = err.find() {
-                return Ok::<_, Infallible>(warp::reply::with_status(
-                    err.to_string(),
-                    StatusCode::BAD_REQUEST,
-                ));
-            }
-
-            Ok(warp::reply::with_status(
-                "INTERNAL SERVER ERROR".to_string(),
-                StatusCode::INTERNAL_SERVER_ERROR,
-            ))
-        });
-
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,30 +1,31 @@
-use async_graphql::{Context, Object};
+use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema};
 
+pub type FinthnifSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn drivers(&self, _ctx: &Context<'_>) -> Vec<Driver> {
-        unimplemented!()
+    async fn driver(&self, ctx: &Context<'_>) -> Driver {
+        ctx.data_unchecked::<Driver>().clone()
     }
 }
 
 #[derive(Clone)]
 pub struct Driver {
-    first_name: String,
+    firstname: String,
 }
 
 #[Object]
 impl Driver {
-    async fn first_name(&self) -> String {
-        self.first_name.clone()
+    async fn firstname(&self) -> String {
+        self.firstname.clone()
     }
 }
 
 impl Default for Driver {
     fn default() -> Self {
         Driver {
-            first_name: "Benoit".to_string(),
+            firstname: "Benoit".to_string(),
         }
     }
 }
